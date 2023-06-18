6783cc1d20525db847c2d7bb070ca76836e6cbaf
omkar <omkar66527@gmail.com>
2023-06-18 11:42:50 +0530

    api-email-integration

---
+++ GetApiandMailIntegration/src/main/java/com/omkar/GetApiandMailIntegration/GetApiandMailIntegrationApplication.java
@@ -0,0 +1,14 @@
+package com.omkar.GetApiandMailIntegration;
+
+import org.omkar.MailHandler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetApiandMailIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetApiandMailIntegrationApplication.class, args);
+	}
+
+}
